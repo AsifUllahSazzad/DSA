703882f63e0d3f96030a169ceb166cc4fa6736a3
Asif Ullah <71225756+AsifUllahSazzad@users.noreply.github.com>
2023-11-09 11:29:18 +0600

    Dictionary Selection Sort

---
+++ Selection Sort Ex-1.py
@@ -0,0 +1,27 @@
+def Selection_Sort(element):
+    for i in range(len(element)-1):
+        min_index = i
+        for j in range(min_index+1,len(element)):
+            if element[min_index]['First Name']>element[j]['First Name']:
+                element[min_index],element[j] = element[j],element[min_index]
+    return element
+element = [
+    {'First Name': 'Raj', 'Last Name': 'Nayyar'},
+    {'First Name': 'Suraj', 'Last Name': 'Sharma'},
+    {'First Name': 'Karan', 'Last Name': 'Kumar'},
+    {'First Name': 'Jade', 'Last Name': 'Canary'},
+    {'First Name': 'Raj', 'Last Name': 'Thakur'},
+    {'First Name': 'Raj', 'Last Name': 'Sharma'},
+    {'First Name': 'Kiran', 'Last Name': 'Kamla'},
+    {'First Name': 'Armaan', 'Last Name': 'Kumar'},
+    {'First Name': 'Jaya', 'Last Name': 'Sharma'},
+    {'First Name': 'Ingrid', 'Last Name': 'Galore'},
+    {'First Name': 'Jaya', 'Last Name': 'Seth'},
+    {'First Name': 'Armaan', 'Last Name': 'Dadra'},
+    {'First Name': 'Ingrid', 'Last Name': 'Maverick'},
+    {'First Name': 'Aahana', 'Last Name': 'Arora'}
+]
+
+ans = Selection_Sort(element)
+for element in ans:
+    print(element)
